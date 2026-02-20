5eb66d69904d58a4c95f4860e7680b60478a589e
Jake Soenneker <jake@soenneker.com>
2026-02-19 19:51:38 -0600

    fixes build

--- test/Soenneker.Lucide.Runners.Icons.Tests/Fixture.cs
+++ test/Soenneker.Lucide.Runners.Icons.Tests/Fixture.cs
@@ -24,7 +24,5 @@ public sealed class Fixture : UnitFixture
 
         IConfiguration config = TestUtil.BuildConfig();
         services.AddSingleton(config);
-
-        services.AddLucideIconsRunnerAsScoped();
     }
 }
