9382b3356dfd66c45c26b1700b7bb88817381405
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:02 -0500

    xml commenting

--- src/Soenneker.Lucide.Runners.Icons/Constants.cs
+++ src/Soenneker.Lucide.Runners.Icons/Constants.cs
@@ -1,8 +1,20 @@
 namespace Soenneker.Lucide.Runners.Icons;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The target repository.
+    /// </summary>
     public const string TargetRepository = "soenneker.lucide.icons";
+    /// <summary>
+    /// The upstream repository url.
+    /// </summary>
     public const string UpstreamRepositoryUrl = "https://github.com/lucide-icons/lucide.git";
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Lucide.Icons";
 }
